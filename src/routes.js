2f13e5132ff0e10ce0d4656cf53dc76264004394
Jorge Albornos <jorgea306@gmail.com>
2022-09-01 12:10:10 -0300

    First Commit

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import HelloWorld from './components/HelloWorld.vue';
+import Login from './components/Login.vue';
+
+export default [
+    {
+        path: '/HelloWorld',
+        component: HelloWorld
+    },
+    {
+        path: '/Login',
+        component: Login
+    },
+]
